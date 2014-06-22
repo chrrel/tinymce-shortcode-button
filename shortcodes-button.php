1a63dfd1e9aceb46ba32b32346c914082352ca11
chrrel <christian.rellmann@gmx.de>
2014-06-22 20:41:10 +0200

    Corrections

--- shortcodes-button.php
+++ shortcodes-button.php
@@ -1,31 +1,27 @@
 <?php
-// Hooks your functions into the correct filters
+
 function shortcode_add_mce_button()
 {
-	// check user permissions
-	if ( !current_user_can( 'edit_posts' ) && !current_user_can( 'edit_pages' ) )
+	if ( !current_user_can( 'edit_posts' ) && !current_user_can( 'edit_pages' ) )	// check user permissions
 	{
 		return;
 	}
-	// check if WYSIWYG is enabled
-	if ( 'true' == get_user_option( 'rich_editing' ) )
+
+	if ( 'true' == get_user_option( 'rich_editing' ) ) 	// WYSIWYG enabled?
 	{
 		add_filter( 'mce_external_plugins', 'shortcode_add_tinymce_plugin' );
 		add_filter( 'mce_buttons', 'shortcode_register_mce_button' );
 	}
 }
-
 add_action('admin_head', 'shortcode_add_mce_button');
 
-// Declare script for new button
-function shortcode_add_tinymce_plugin( $plugin_array )
+function shortcode_add_tinymce_plugin( $plugin_array ) // script for the new button
 {
 	$plugin_array['shortcode_mce_button'] =  get_stylesheet_directory_uri() .'/js/mce-button.js';
 	return $plugin_array;
 }
 
-// Register new button in the editor
-function shortcode_register_mce_button( $buttons )
+function shortcode_register_mce_button( $buttons ) // register the new button
 {
 	array_push( $buttons, 'shortcode_mce_button' );
 	return $buttons;
